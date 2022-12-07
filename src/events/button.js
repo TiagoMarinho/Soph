8257bd9b2697ba589c08f6067d50f6321241aebb
Tiago Marinho <tiagomeraki@gmail.com>
2022-12-07 15:29:36 -0300

    separate button interaction events from commands

---
+++ src/events/button.js
@@ -0,0 +1,52 @@
+import { Events, EmbedBuilder } from 'discord.js'
+import { generate } from '../commands/artificial intelligence/dream/execute.js'
+import languages from '../locale/languages.js'
+import config from '../../config.json' assert { type: 'json' }
+
+export default {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+
+		if (!interaction.isButton())
+			return
+		
+		try {
+			await interaction.deferUpdate()
+		} catch (error) {
+			console.error(error)
+
+			return
+		}
+
+		const message = await interaction.fetchReply()
+		const embeds = message.embeds.map(embed => EmbedBuilder.from(embed))
+
+		if (interaction.customId === 'repeat') {
+			const cacheChannelId = config.cacheChannelId
+			const cacheChannel = await interaction.client.channels.cache.get(cacheChannelId)
+
+			const cacheMessageId = embeds[0].data.url.match(/\/(\d+)$/)[1]
+			const cacheMessage = await cacheChannel.messages.fetch(cacheMessageId)
+
+			const parameters = JSON.parse(cacheMessage.content.match(/^```json\n(.+)```$/)[1])
+
+			const reply = await interaction.followUp({
+				content: languages[interaction.locale]?.["generating images"] ?? `Generating...`
+			})
+
+			generate(interaction, parameters, reply)
+			return
+		}
+
+		switch (interaction.customId) {
+			case `next`:
+				embeds.push(embeds.shift())
+				break
+			case `previous`:
+				embeds.unshift(embeds.pop())
+				break
+		}
+
+		await interaction.editReply({ embeds: embeds })
+	},
+}
---
+++ src/events/command.js
@@ -0,0 +1,31 @@
+import { Events, EmbedBuilder } from 'discord.js'
+import { generate } from '../commands/artificial intelligence/dream/execute.js'
+import languages from '../locale/languages.js'
+import config from '../../config.json' assert { type: 'json' }
+
+export default {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+
+		if (!interaction.isCommand()) 
+			return
+
+		const command = interaction.client.commands.get(interaction.commandName)
+
+		if (!command) return
+
+		const fullUserTag = `${interaction.user.username}#${interaction.user.discriminator}`
+		console.log(`${fullUserTag} used ${interaction}`)
+
+		try {
+			await command.execute(interaction)
+		} catch (error) {
+			console.error(error)
+
+			if (error.code === 10062) // bot took too long to respond/defer, trying to reply would crash it
+				return
+
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+		}
+	},
+}
