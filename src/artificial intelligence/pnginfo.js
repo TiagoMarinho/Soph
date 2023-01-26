236dff40ddb5bc72c1c735137254ed43dadaf4f9
Tiago Marinho <tiagomeraki@gmail.com>
2023-01-26 03:41:08 -0300

    make `/metadata` more robust, fix #18, fix #19
    also removes png-chunk dependency

--- src/artificial intelligence/pnginfo.js
+++ src/artificial intelligence/pnginfo.js
@@ -1,8 +1,33 @@
-export const parseParameters = textChunk => {
-	const parts = textChunk.split(`\n`)
-	const prompt = `Prompt: ${parts[0]}`
-	const negative = parts[1]
-	const settings = parts[2].split(`, `)
+import servers from './serverlist.json' assert { type: 'json' }
+
+export const parseParameters = async imageBuffer => {
+	
+	const base64InputImage = `data:image/png;base64,${Buffer.from(imageBuffer).toString('base64')}`
+	const payload = {
+		"image": base64InputImage
+	}
+	const apiEndpoint = `${servers[0].address}/sdapi/v1/png-info`
+	const buff = Buffer.from(servers[0].credentials, 'utf-8')
+	const base64Credentials = buff.toString('base64')
+	const response = await fetch(apiEndpoint, {
+		method: 'post',
+		body: JSON.stringify(payload),
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Basic ${base64Credentials}`
+		}
+	})
+	const data = await response.json()
+	
+	if (!data.info?.length)
+		return []
+
+	const parts = data.info.split(`\n`)
+	const negativePromptStartIndex = parts.findLastIndex(item => item.startsWith(`Negative prompt:`))
+
+	const prompt = `Prompt: ${parts.slice(0, negativePromptStartIndex).join(`\n`)}`
+	const negative = parts.slice(negativePromptStartIndex, -1).join(`\n`)
+	const settings = parts.at(-1).split(`, `)
 
 	const parameters = [prompt, negative, ...settings]
 
@@ -10,7 +35,7 @@ export const parseParameters = textChunk => {
 	
 	for (const parameter of parameters) {
 		const [key, value] = parameter.split(`: `)
-		pngInfo.push({name: key, value: value})
+		pngInfo.push({ name: key, value: value })
 	}
 	return pngInfo
 }
