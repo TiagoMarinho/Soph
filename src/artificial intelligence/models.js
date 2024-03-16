dfd8b856ea05f446d384458ce985026b55b1a298
Edoan <edoan.rodrigues@gmail.com>
2024-03-16 19:00:49 -0300

    print response when no models could be found

--- src/artificial intelligence/models.js
+++ src/artificial intelligence/models.js
@@ -6,4 +6,8 @@ const response = await fetch("http://127.0.0.1:7860/sdapi/v1/sd-models", {
 })
 const data = await response.json()
 
+if (!Array.isArray(data)) {
+	console.log('could not get models, /v1/sd-models did not return an array. Response:\n', data);
+}
+
 export default data
