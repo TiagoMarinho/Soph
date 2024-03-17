8e29458e47e85eba85a02dd0688bc0e1f95b30a6
TiagoMarinho <tiagomeraki@gmail.com>
2024-03-17 03:29:48 +0100

    refactor to use console.assert

--- src/artificial intelligence/models.js
+++ src/artificial intelligence/models.js
@@ -5,11 +5,9 @@ const response = await fetch("http://127.0.0.1:7860/sdapi/v1/sd-models", {
 	}
 })
 
-let data = await response.json()
+const data = await response.json()
 
-if (!Array.isArray(data)) {
-	console.warn('could not get models, /v1/sd-models did not return an array. Response:\n', data)
-	data = []
-}
+const isDataAnArray = Array.isArray(data)
+console.assert(isDataAnArray, `Could not get models, /v1/sd-models did not return an array. Response:\n${data}`)
 
-export default data
+export default isDataAnArray ? data : []
